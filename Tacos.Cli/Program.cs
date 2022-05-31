e1534057d7f3b61d0a12e7d1dbb22833e6653b0d
Brian Donaldson <brian@bitobrian.com>
2022-05-30 19:14:54 -0500

    Add fancy cli tooling.

--- Tacos.Cli/Program.cs
+++ Tacos.Cli/Program.cs
@@ -1,3 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// dotnet tool install --global --add-source ./nupkg tacos.cli
 
-// dotnet tool install --global --add-source ./nupkg tacos.cli
+using Spectre.Console;
+using System.CommandLine;
+
+var shellOption = new Option<string>(name: "--shell", description: "Choose either hard or soft", getDefaultValue: () => "hard");
+
+var rootCommand = new RootCommand("A taco CLI tool to show how to make CLI tacos.");
+
+var newCommand = new Command("new", "Create a new taco.")
+{
+    shellOption
+};
+
+rootCommand.Add(newCommand);
+
+newCommand.SetHandler((string shell) =>
+{
+    DoAThing(shell);
+}, shellOption);
+
+AnsiConsole.Write(
+    new FigletText("Taco CLI")
+    .Color(Color.SandyBrown));
+
+return await rootCommand.InvokeAsync(args);
+
+static void DoAThing(string shell)
+{
+    Console.WriteLine($"You chose a {shell} taco.");
+}
